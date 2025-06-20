7dc57f60d6839be0d35916deb3ed4b6a4fddb7f7
Lovmong <1967471283@qq.com>
2025-06-20 16:43:44 +0800

    Refactor disk and IP display with scrolling and error handling

---
+++ pm_auto/oled_pages/disks.py
@@ -0,0 +1,66 @@
+from itertools import islice
+import time
+
+from sf_rpi_status import get_disks_info
+from .uilts import format_bytes
+
+from pathlib import Path
+grandparent_dir = Path(__file__).resolve().parent.parent
+sdcard_icon = str(grandparent_dir) + '/icons/sdcard_icon_20.png'
+nvme_icon = str(grandparent_dir) + '/icons/nvme_icon_20.png'
+usb_stick_icon = str(grandparent_dir) + '/icons/usb_stick_icon_20.png'
+error = str(grandparent_dir) + '/icons/error_icon_20.png'
+
+disk_index = 0
+disk_num = 0
+cycle_time_start = 0
+
+def oled_page_disk(oled, config):
+    global disk_index, disk_num, cycle_time_start
+
+    scroll_interval = config['scroll_interval']
+
+    disks_info = get_disks_info()
+
+    oled.clear()
+
+    if len(disks_info) == 0:
+        oled.draw_icon(error, 0, 0, scale=1, invert=False, dither=False, threshold=50)
+        oled.draw_text('Disk Detection Error', 0, 22, size=14)
+    else:
+        if disk_num != len(disks_info):
+            disk_index = 0
+            cycle_time_start = time.time()
+            disk_num = len(disks_info)
+
+        if time.time() - cycle_time_start >= scroll_interval:
+            cycle_time_start = time.time()
+            disk_index += 3
+            if disk_index >= len(disks_info):
+                disk_index = 0
+
+        _iter = islice(disks_info.items(), disk_index, disk_index + 3)
+
+        for i in range(3):
+            try:
+                name, info = next(_iter)
+                if 'mmcblk' in name:
+                    oled.draw_icon(sdcard_icon, 0, i * 22, dither=False, threshold=130)
+                elif 'nvme0' in name:
+                    oled.draw_icon(nvme_icon, 0, i * 22+5, dither=False, threshold=130)
+                    oled.draw_text(f'ssd 0', 1, i * 22, size=9)
+                elif 'nvme1' in name:
+                    oled.draw_icon(nvme_icon, 0, i * 22+5, dither=False, threshold=130)
+                    oled.draw_text(f'ssd 1', 1, i * 22, size=9)
+                elif 'sda' in name:
+                    oled.draw_icon(usb_stick_icon, 0, i * 22, dither=False, threshold=100)
+
+                _total, _uint = format_bytes(info.total)
+                _used = format_bytes(info._used, _uint)
+                oled.draw_text(f'{_used}/{_total} {_uint}', 32, i * 22, size=12)
+                oled.draw_bar_graph_horizontal(info._percent, 26, i * 23 + 12, 100, 5)              
+            except StopIteration:
+                break
+
+    oled.display()
+    
--- pm_auto/oled_pages/ips.py
+++ pm_auto/oled_pages/ips.py
@@ -1,35 +1,56 @@
-from sf_rpi_status import \
-    get_cpu_temperature, \
-    get_cpu_percent, \
-    get_memory_info, \
-    get_disks_info, \
-    get_ips
+from itertools import islice
 import time
 
-from.oled_config import *
-
+from sf_rpi_status import get_ips
 
 from pathlib import Path
 grandparent_dir = Path(__file__).resolve().parent.parent
 ethernet_icon = str(grandparent_dir) + '/icons/ethernet_icon_20.png'
 wifi_icon = str(grandparent_dir) + '/icons/wifi_icon_20.png'
 net_icon = str(grandparent_dir) + '/icons/net_icon_20.png'
+error = str(grandparent_dir) + '/icons/error_icon_20.png'
 
-def oled_page_ips(oled):
+ip_index = 0
+ip_num = 0
+cycle_time_start = 0
+
+def oled_page_ips(oled, config):
+    global ip_index, ip_num, cycle_time_start
+
+    scroll_interval = config['scroll_interval']
+
     ips = get_ips()
-    ips['lo'] = '127.0.0.1'
+    
     oled.clear()
 
-    i = 0
-    for interface, ip in ips.items():
-        if interface == 'eth0':
-            oled.draw_icon(ethernet_icon, 0, i*22, scale=1, invert=False,  dither=False, threshold=80)
-        elif interface == 'wlan0':
-            oled.draw_icon(wifi_icon, 0, i*22, scale=1, invert=False, dither=False, threshold=85)
-        else:
-            oled.draw_icon(net_icon, 0, i*22, scale=1, invert=False, dither=False, threshold=100)
+    if len(ips) == 0:
+        oled.draw_icon(error, 0, 0, scale=1, invert=False, dither=False, threshold=50)
+        oled.draw_text('DISCONNECTED', 22, 0, size=14)
+    else:
+        if ip_num != len(ips):
+            ip_index = 0
+            cycle_time_start = time.time()
+            ip_num = len(ips)
 
-        oled.draw_text(f'{ip}', 22, i * 22, size=14)
-        i += 1
+        if time.time() - cycle_time_start >= scroll_interval:
+            cycle_time_start = time.time()
+            ip_index += 3
+            if ip_index >= len(ips):
+                ip_index = 0
+
+        _iter = islice(ips.items(), ip_index, ip_index + 3)
+
+        for i in range(3):
+            try:
+                interface, ip = next(_iter)
+                if interface == 'eth0':
+                    oled.draw_icon(ethernet_icon, 0, i*22, scale=1, invert=False,  dither=False, threshold=80)
+                elif interface == 'wlan0':
+                    oled.draw_icon(wifi_icon, 0, i*22, scale=1, invert=False, dither=False, threshold=85)
+                else:
+                    oled.draw_icon(net_icon, 0, i*22, scale=1, invert=False, dither=False, threshold=100)
+                oled.draw_text(f'{ip}', 22, i * 22, size=14)
+            except StopIteration:
+                break
 
     oled.display()
