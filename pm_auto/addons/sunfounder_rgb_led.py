48b8830761a67ee6823bb2b1c6fa04d9d00fb801
Cavon <cavonxx@gmail.com>
2026-01-23 17:18:10 +0800

    refactor(sunfounder_rgb_led): rename config keys for consistency
    Update all RGB LED configuration keys to use 'rgb_' prefix consistently instead of 'rgb_led_' for better naming uniformity

--- pm_auto/addons/sunfounder_rgb_led.py
+++ pm_auto/addons/sunfounder_rgb_led.py
@@ -46,56 +46,56 @@ class SunFounderRGBLEDAddon(Addon):
                 self.count = _count
                 patch['rgb_led_count'] = _count
                 self.log.debug(f"Update LED count: {_count}")
-        if 'rgb_led_enable' in config:
-            _enable = config['rgb_led_enable']
+        if 'rgb_enable' in config:
+            _enable = config['rgb_enable']
             if not isinstance(_enable, bool):
-                self.log.error(f"Invalid rgb_led_enable: {_enable}")
+                self.log.error(f"Invalid rgb_enable: {_enable}")
             else:
                 if not init:
                     self.rgb.set_enable(_enable)
                 self.enable = _enable
-                patch['rgb_led_enable'] = _enable
+                patch['rgb_enable'] = _enable
                 self.log.debug(f"Update RGB enable: {_enable}")
-        if 'rgb_led_color' in config:
-            _color = config['rgb_led_color']
+        if 'rgb_color' in config:
+            _color = config['rgb_color']
             if not isinstance(_color, str):
-                self.log.error(f"Invalid rgb_led_color: {_color}")
+                self.log.error(f"Invalid rgb_color: {_color}")
             else:
                 if not init:
                     self.rgb.set_color(_color)
                 self.color = _color
-                patch['rgb_led_color'] = _color
-                self.log.debug(f"Update RGB LED color: {_color}")
-        if 'rgb_led_brightness' in config:
-            _brightness = config['rgb_led_brightness']
+                patch['rgb_color'] = _color
+                self.log.debug(f"Update RGB color: {_color}")
+        if 'rgb_brightness' in config:
+            _brightness = config['rgb_brightness']
             if not isinstance(_brightness, int):
-                self.log.error(f"Invalid rgb_led_brightness: {_brightness}")
+                self.log.error(f"Invalid rgb_brightness: {_brightness}")
             else:
                 if not init:
                     self.rgb.set_brightness(_brightness)
                 self.brightness = _brightness
-                patch['rgb_led_brightness'] = _brightness
-                self.log.debug(f"Update RGB LED brightness: {_brightness}")
-        if 'rgb_led_speed' in config:
-            _speed = config['rgb_led_speed']
+                patch['rgb_brightness'] = _brightness
+                self.log.debug(f"Update RGB brightness: {_brightness}")
+        if 'rgb_speed' in config:
+            _speed = config['rgb_speed']
             if not isinstance(_speed, int):
-                self.log.error(f"Invalid rgb_led_speed: {_speed}")
+                self.log.error(f"Invalid rgb_speed: {_speed}")
             else:
                 if not init:
                     self.rgb.set_speed(_speed)
                 self.speed = _speed
-                patch['rgb_led_speed'] = _speed
-                self.log.debug(f"Update RGB LED speed: {_speed}")
-        if 'rgb_led_style' in config:
-            _style = config['rgb_led_style']
+                patch['rgb_speed'] = _speed
+                self.log.debug(f"Update RGB speed: {_speed}")
+        if 'rgb_style' in config:
+            _style = config['rgb_style']
             if not isinstance(_style, str) or _style not in RGB_STYLES:
-                self.log.error(f"Invalid rgb_led_style: {_style}")
+                self.log.error(f"Invalid rgb_style: {_style}")
             else:
                 if not init:
                     self.rgb.set_style(_style)
                 self.style = _style
-                patch['rgb_led_style'] = _style
-                self.log.debug(f"Update RGB LED style: {_style}")
+                patch['rgb_style'] = _style
+                self.log.debug(f"Update RGB style: {_style}")
         return patch
 
     @log_error
